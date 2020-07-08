0ed97f9f2b122a63b7fe0faa2ca5ee5fe16962b4
Lucas <61209498+CompileException@users.noreply.github.com>
2020-07-08 16:00:56 +0200

    first commit
    initial commit

--- BungeeBridge/src/main/java/de/teamholy/bridge/events/Events.java
+++ BungeeBridge/src/main/java/de/teamholy/bridge/events/Events.java
@@ -1,5 +1,6 @@
 package de.teamholy.bridge.events;
 
+import de.teamholy.bridge.main.BungeeBridge;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,7 +12,9 @@ public class Events implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        
+
+        player.sendMessage(BungeeBridge."");
+
 
 
     }
